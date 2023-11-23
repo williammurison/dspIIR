e9fba346d59005de231348f487024c823eba3aef
williammurison <146353513+williammurison@users.noreply.github.com>
2023-11-23 16:39:32 +0000

    midi

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 
 from pyfirmata2 import Arduino
 
-import pyaudio
-import time
+import rtmidi
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 from plottingWindow import RealtimePlotWindow
 
@@ -23,21 +21,39 @@ realtimePlotWindow3 = RealtimePlotWindow()
 # sampling rate: 100Hz
 samplingRate = 100
 
-p = pyaudio.PyAudio()
+#synth connection
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
 
-volume = 0.5  # range [0.0, 1.0]
-fs = 44100  # sampling rate, Hz, must be integer
-f = 440.0  # sine frequency, Hz, may be float
+if available_ports:
+    midiout.open_port(0)
+else:
+    midiout.open_virtual_port("My virtual output")
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
+threshold = 0.5
 
-# generate samples, note conversion to float32 array
-#1 period of the sine wav
-samples = (volume * (np.sin(2 * np.pi * np.arange(fs / f) * f / fs)).astype(np.float32)).tobytes()
+C_on = [0x90, 60, 112]
+C_off = [0x80, 60, 50]
+
+E_on = [0x90, 64, 112]
+E_off = [0x80, 64, 50]
+
+G_on = [0x90, 67, 112]
+G_off = [0x80, 67, 50]
+
+c_on = [0x90, 72, 112]
+c_off = [0x80, 72, 50]
+
+cc_message = [0xB0, 1, 0]
+midiout.send_message(cc_message)
+
+index_down = False
+middle_down = False
+ring_down = False
+pinky_down = False
+
+#Automation
+automationThreshold = 0.7
 
 # called for every new sample which has arrived from the Arduino
 def callBack0(data):
@@ -45,15 +61,91 @@ def callBack0(data):
     # add any filtering here:
     # data = self.myfilter.dofilter(data)
 
+    global index_down
+
+    # note is off
+    if not index_down:
+        # check to see if its over the threshold and if it is turn it on
+        if data > threshold:
+            midiout.send_message(C_on)
+            index_down = True
+
+    # note is on
+    else:
+        # check to see if note is under threshold and if it is turn it off
+        if data < threshold:
+            midiout.send_message(C_off)
+            index_down = False
+
+        if data > automationThreshold:
+
+            automationRange = 1 - automationThreshold
+            #convert from range of data to range of midi (0 - 0.3 to 0 - 126)
+            mod = int((data - automationThreshold) / automationRange * 126)
+
+            mod_msg = [0xB0, 1, mod]
+            midiout.send_message(mod_msg)
+
     realtimePlotWindow0.addData(data)
 
 def callBack1(data):
+    
+    global middle_down
+
+    # note is off
+    if not middle_down:
+        # check to see if its over the threshold and if it is turn it on
+        if data > threshold:
+            midiout.send_message(E_on)
+            middle_down = True
+
+    # note is on
+    else:
+        # check to see if note is under threshold and if it is turn it off
+        if data < threshold:
+            midiout.send_message(E_off)
+            middle_down = False
+
     realtimePlotWindow1.addData(data)
 
 def callBack2(data):
+    
+    global ring_down
+
+    # note is off
+    if not ring_down:
+        # check to see if its over the threshold and if it is turn it on
+        if data > threshold:
+            midiout.send_message(G_on)
+            ring_down = True
+
+    # note is on
+    else:
+        # check to see if note is under threshold and if it is turn it off
+        if data < threshold:
+            midiout.send_message(G_off)
+            ring_down = False
+
     realtimePlotWindow2.addData(data)
 
 def callBack3(data):
+    
+    global pinky_down
+
+    # note is off
+    if not pinky_down:
+        # check to see if its over the threshold and if it is turn it on
+        if data > threshold:
+            midiout.send_message(c_on)
+            pinky_down = True
+
+    # note is on
+    else:
+        # check to see if note is under threshold and if it is turn it off
+        if data < threshold:
+            midiout.send_message(c_off)
+            pinky_down = False
+
     realtimePlotWindow3.addData(data)
 
 # Get the Ardunio board.
@@ -75,23 +167,7 @@ board.analog[2].enable_reporting()
 board.analog[3].register_callback(callBack3)
 board.analog[3].enable_reporting()
 
-t = 0 #track how far along the sine wave we are
-
 plt.show()
-
-# while True:
-#     if currentValue1 < 0.5:
-#         if t % len(samples) == 0:
-#             # play. May repeat with different volume values (if done interactively)
-#             start_time = time.time()
-#             stream.write(samples)
-#             print("Played sound for {:.5f} seconds".format(time.time() - start_time))
-#         t += 1
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
 
 # needs to be called to close the serial port
 board.exit()
